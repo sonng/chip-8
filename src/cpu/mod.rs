536c5723e7e051232b416b539a62eee50ce56316
Son Nguyen <anh.s.nguyen@gmail.com>
2019-11-14 04:37:23 +0000

    Implemented AND and wrote unit test for it

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -10,6 +10,7 @@ const END_PROGRAM: u8 = 0x0 as u8;
 const REGISTER_OPERATION: u8 = 0x8 as u8;
 const REGISTER_STORE: u8 = 0x0 as u8;
 const REGISTER_OR: u8 = 0x1 as u8;
+const REGISTER_AND: u8 = 0x2 as u8;
 
 pub struct CPU {
     registers: [u8; 16],
@@ -64,6 +65,7 @@ impl CPU {
                     match op_action {
                         REGISTER_STORE => { self.copy(reg_x as usize, reg_y as usize); },
                         REGISTER_OR => { self.or(reg_x as usize, reg_y as usize); },
+                        REGISTER_AND => { self.and(reg_x as usize, reg_y as usize); },
                         _ => unimplemented!("No imple for {:04x} - {:04x}", op_code, op_action),
                     }
 
--- src/cpu/register_operations.rs
+++ src/cpu/register_operations.rs
@@ -1,13 +1,16 @@
 use super::CPU;
 
 impl CPU {
-
     pub(super) fn copy(&mut self, x: usize, y: usize) {
         self.registers[x] = self.registers[y];
     }
 
     pub(super) fn or(&mut self, x: usize, y: usize) {
         self.registers[x] = self.registers[x] | self.registers[y]
+    }
+
+    pub(super) fn and(&mut self, x: usize, y: usize) {
+        self.registers[x] = self.registers[x] & self.registers[y]
     }
 }
 
@@ -49,6 +52,25 @@ mod tests {
 
         chip8.run();
         assert_eq!(chip8.registers[0], 0b11111111 as u8);
+        assert_eq!(chip8.registers[1], 0b10101010 as u8);
+    }
+
+    #[test]
+    fn test_and_register() {
+        let mut chip8 = CPU::new();
+        let mut test = chip8.blank_program();
+
+        chip8.registers[0] = 0b01010101 as u8;
+        chip8.registers[1] = 0b10101010 as u8;
+
+        test[0] = 0x80 as u8; test[1] = 0x12 as u8;
+
+        chip8.load(test);
+        assert_eq!(chip8.registers[0], 0b01010101 as u8);
+        assert_eq!(chip8.registers[1], 0b10101010 as u8);
+
+        chip8.run();
+        assert_eq!(chip8.registers[0], 0b00000000 as u8);
         assert_eq!(chip8.registers[1], 0b10101010 as u8);
     }
 }
